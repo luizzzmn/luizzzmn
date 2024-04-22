204680b899452e40bb875b5c3e398bfa37e17023
Luiz <134730692+luizzzmn@users.noreply.github.com>
2024-04-22 11:28:35 -0300

    Operações Matrizes.java

--- Operações Matrizes.java
+++ Operações Matrizes.java
@@ -1,4 +1,26 @@
+package matrizes;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 public class Matriz {
+	
+	public static void main(String[] args) {
+			
+			Result result = JUnitCore.runClasses(MatrizTest.class);
+	
+	        for (Failure failure : result.getFailures()) {
+	            System.out.println(failure.toString());
+	        }
+	
+	        if (result.wasSuccessful()) {
+	            System.out.println("Todos os testes passaram!");
+	        } else {
+	            System.out.println("Alguns testes falharam.");
+	        	
+	        }
+		}
 
     public int[][] soma(int x[][], int y[][]) {
         if (x.length != y.length || x[0].length != y[0].length) {
